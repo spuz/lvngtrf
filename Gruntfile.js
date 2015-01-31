b728676a42f8e0c2126a2e88cc5f20b0fe662b6b
Bernhard Benke <benke@ultrasmall.at>
2015-01-31 01:06:23 +0100

    Update Gruntfile.js
    Add Grunt tasks for
    * build Jekyll
    * compile Compass
    * enable watch
    * enable to run grunt tasks concurrent

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,72 @@ module.exports = function(grunt) {
 
     pkg: grunt.file.readJSON('package.json'),
 
+    jekyll: {
+      build: {
+        options: 'config.yml'
+      }
+    },
+
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css',
+          environment: 'production',
+          outputStyle: 'compressed',
+          noLineComments: true,
+          watch: false
+        }
+      },
+      dev: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css',
+          environment: 'development',
+          outputStyle: 'expanded',
+          noLineComments: false,
+          watch: true
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      jekyll: {
+        files: [
+          '_includes/*.html',
+          '_posts/*.markdown',
+          '_posts/*.md',
+          'index.html',
+          '_config.yml',
+          'css/*.css'
+        ],
+        tasks: ['jekyll']
+      }
+    },
+
+    concurrent: {
+      target: {
+        tasks: [
+          'compass:dev',
+          'watch'
+        ],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    }
+
   });
 
 
   // grunt.loadNpmTasks('grunt-jekyll');
   // grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', [
+    'concurrent:target',
+    'compass'
+  ]);
 }
